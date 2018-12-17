07b64602666a2188b65ff2c055cfabdf2cfa75c1
abrockmeier <alexander.brockmeier@ambicotech.com>
2018-12-17 15:08:06 +0100

    v0.1
    main added;

--- Koordinaten_Punkte_C-sharp/Program.cs
+++ Koordinaten_Punkte_C-sharp/Program.cs
@@ -10,6 +10,16 @@ namespace Koordinaten_Punkte_C_sharp
 	{
 		static void Main(string[] args)
 		{
+		CPunkt a = new CPunkt(10, 20);
+		CPunkt b = new Cpunkt(a);
+		CPunkt c = new CPunkt();
+
+		c.X = 30;
+		c.Y = 40;
+
+		Console.WriteLine("Punkt B<" + b.X + "|" + b.Y +">");
+		Console.WriteLine("Abstand von Punkt b zu <0|0>: " + b.Abstand());
 		}
+		
 	}
 }
